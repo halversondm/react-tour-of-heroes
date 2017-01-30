c8e2799dcd2073e39af5530ab09d5df03513a10b
Dan Halverson <daniel.m.halverson@gmail.com>
2017-01-30 06:58:35 -0600

    removing files

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import "./styles/main.css";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import TourOfHeroes from "./components/TourOfHeroes";
-
-ReactDOM.render(<TourOfHeroes />, document.getElementById("anchor"));
